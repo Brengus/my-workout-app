a525e7cf1338d5a86a60b6997576ffce3b40dcf6
Brengus <giorgibregvadze2204@gmail.com>
2025-12-15 21:09:14 +0400

    adding responsive

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,4 @@
+import { set } from "date-fns";
 import "../css/navigation.css";
 import React, { useEffect, useState, useRef } from "react";
 
@@ -6,6 +7,7 @@ import React, { useEffect, useState, useRef } from "react";
 function Navigation() {
     const scrollRef = useRef<HTMLDivElement>(null);
     const ghostRef = useRef<HTMLDivElement>(null);
+    const [menuOpen, setMenuOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
 
     const handleClick = (sectionId: string) => {
@@ -15,6 +17,13 @@ function Navigation() {
         } else {
             console.warn(`Section with ID ${sectionId} not found.`);
         }
+        if (menuOpen) {
+            setMenuOpen(false);
+        }
+    }
+
+    const handleMenuClick = () => {
+        setMenuOpen(!menuOpen);
     }
 
     const handleScroll = () => {
@@ -40,12 +49,18 @@ function Navigation() {
     return (
         <>
             <div className="ghost" ref={ghostRef}></div>
+
             <div className={`main ${isScrolled ? "active" : ""}`} ref={scrollRef}>
+                <div className="menu-icon" onClick={() => handleMenuClick()}>
+                    <div className="first-stick"></div>
+                    <div className="second-stick"></div>
+                    <div className="third-stick"></div>
+                </div>
                 <nav className="navigation-class">
                     <div className="item">
                         <img src="../logo.svg" alt="logo image" className="logo" onClick={() => handleClick("home")} />
                     </div>
-                    <div className="nav-buttons item">
+                    <div className={`nav-buttons item ${menuOpen ? "open" : ""}`}>
                         <button className={`item-buttons ${isScrolled ? "active" : ""}`} onClick={() => handleClick("home")}>HOME</button>
                         <button className={`item-buttons ${isScrolled ? "active" : ""}`} onClick={() => handleClick("aboutus")}>ABOUT US</button>
                         <button className={`item-buttons ${isScrolled ? "active" : ""}`} onClick={() => handleClick("testimonials")}>TESTIMONIALS</button>
